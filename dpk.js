0bce457774c6441623e76e50403bc276087f37bc
Aleksey Barabanov <forarbaiten@yandex.ru>
2023-03-01 00:45:29 +0100

    refactoring made

--- dpk.js
+++ dpk.js
@@ -3,24 +3,21 @@ const crypto = require("crypto");
 exports.deterministicPartitionKey = (event) => {
   const TRIVIAL_PARTITION_KEY = "0";
   const MAX_PARTITION_KEY_LENGTH = 256;
-  let candidate;
 
-  if (event) {
-    if (event.partitionKey) {
-      candidate = event.partitionKey;
-    } else {
-      const data = JSON.stringify(event);
-      candidate = crypto.createHash("sha3-512").update(data).digest("hex");
-    }
+  let candidate = TRIVIAL_PARTITION_KEY; //here I delete one if..else operation
+
+  if (event && event.partitionKey) { //it makes the code shorter, but I don't think it easier to read and understand
+    candidate = event.partitionKey;
+  }
+  else if (event) {
+    const data = JSON.stringify(event);
+    candidate = crypto.createHash("sha3-512").update(data).digest("hex");
   }
 
-  if (candidate) {
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
-    }
-  } else {
-    candidate = TRIVIAL_PARTITION_KEY;
+  if (typeof candidate !== "string") {
+    candidate = JSON.stringify(candidate);
   }
+
   if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
     candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
   }
